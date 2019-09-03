b3b41d2d88047b6dcbfc99e720083a2c99e9c0ca
Ilari Marjamaa <t7mail00@students.oamk.fi>
2019-09-03 18:32:10 +0300

    Initial Commit

---
+++ Lab2.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab2.3
+//
+//  Created by Ilari Marjamaa on 03/09/2019.
+//  Copyright © 2019 Ilari Marjamaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
